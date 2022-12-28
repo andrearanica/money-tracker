f4655a34c9c77c2e832c7706b87148ad9ba408e0
andrearanica <andrearanica2004@gmail.com>
2022-12-28 16:03:05 +0100

    🚧 Delete function

--- backend/controllers/movements.js
+++ backend/controllers/movements.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 import { Movement } from '../models/movement.js'
 
@@ -36,5 +37,19 @@ export const getMovementsByEmail = async (req, res) => {
         res.status(200).json(result)
     } catch (error) {
         res.status(404).json({ message: error.message })
+    }
+}
+
+export const deleteMovement = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: 'Invalid ID' })
+    } else {
+        try {
+            await Movement.findByIdAndDelete(id)
+            res.status(201).json({ message: 'Moviment deleted' })
+        } catch (error) {
+            res.status(404).json({ error: error.message })
+        }
     }
 }
--- backend/routes/movements.js
+++ backend/routes/movements.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getAllMovements, insertMovement, getMovementsByEmail } from '../controllers/movements.js'
+import { getAllMovements, insertMovement, getMovementsByEmail, deleteMovement } from '../controllers/movements.js'
 
 const router = express.Router()
 
 router.get('/', getAllMovements)
 router.get('/:email', getMovementsByEmail)
 router.post('/', insertMovement)
+router.delete('/:id', deleteMovement)
 
 export default router
